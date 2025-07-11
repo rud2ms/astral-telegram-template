4c6b68d536c6cb09eae74bda59b6a49c766ff4be
rud2ms <rud9ms@gmail.com>
2025-07-11 09:42:49 +0900

    Delete strategy.py

--- strategy.py
+++
@@ -1,7 +0,0 @@
-from utils import send_telegram_message
-
-def should_exit_position(rsi):
-    if rsi > 70:
-        send_telegram_message(f"📈 RSI: {rsi} - 익절 조건 만족. 포지션 종료 권고")
-        return True
-    return False
